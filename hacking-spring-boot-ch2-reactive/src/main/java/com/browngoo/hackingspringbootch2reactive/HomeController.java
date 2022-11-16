1e3393f18f2275553da0b40bc43df17b4a3d2a6d
Deoksoo(Eden) <deoksoo.lee@smartfoodnet.com>
2022-11-16 13:42:48 +0900

    Mongo setting

---
+++ hacking-spring-boot-ch2-reactive/src/main/java/com/browngoo/hackingspringbootch2reactive/HomeController.java
@@ -0,0 +1,36 @@
+package com.browngoo.hackingspringbootch2reactive;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.reactive.result.view.Rendering;
+import reactor.core.publisher.Mono;
+
+@Controller
+public class HomeController {
+
+    private ItemRepository itemRepository;
+    private CartRepository cartRepository;
+
+    public HomeController(ItemRepository itemRepository, CartRepository cartRepository) {
+        this.itemRepository = itemRepository;
+        this.cartRepository = cartRepository;
+    }
+
+    @GetMapping
+    Mono<Rendering> home(){
+        return Mono.just(Rendering.view("home.html")
+            .modelAttribute("items", this.itemRepository.findAll())
+            .modelAttribute("cart", this.cartRepository.findById("My Cart").defaultIfEmpty(new Cart("My Cart")))
+            .build());
+    }
+//@GetMapping
+//Mono<Rendering> home() { // <1>
+//    return Mono.just(Rendering.view("home.html") // <2>
+//        .modelAttribute("items", //
+//            this.itemRepository.findAll()) // <3>
+//        .modelAttribute("cart", //
+//            this.cartRepository.findById("My Cart") // <4>
+//                .defaultIfEmpty(new Cart("My Cart")))
+//        .build());
+//}
+}
