2674382efedff6860b0d03f5f5dd253d4c3f2202
Eden.lee home <lds2292@gmail.com>
2022-11-19 23:39:39 +0900

    blockhound added

--- hacking-spring-boot-ch3-reactive/src/main/java/com/browngoo/hackingspringbootch3reactive/HackingSpringBootCh3ReactiveApplication.java
+++ hacking-spring-boot-ch3-reactive/src/main/java/com/browngoo/hackingspringbootch3reactive/HackingSpringBootCh3ReactiveApplication.java
@@ -2,11 +2,19 @@ package com.browngoo.hackingspringbootch3reactive;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.thymeleaf.TemplateEngine;
+import reactor.blockhound.BlockHound;
 
 @SpringBootApplication
 public class HackingSpringBootCh3ReactiveApplication {
 
     public static void main(String[] args) {
+
+        BlockHound.builder()
+                .allowBlockingCallsInside(
+                    TemplateEngine.class.getCanonicalName(), "process"
+                ).install();
+
         SpringApplication.run(HackingSpringBootCh3ReactiveApplication.class, args);
     }
 
